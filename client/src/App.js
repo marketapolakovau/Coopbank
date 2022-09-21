da1fab0775d222f10d99ac1e1c5119da0ec16821
marketapolakovau <marketa.polakova@unicorn.com>
2022-09-21 15:39:16 +0200

    Marketa commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
+import { UserProvider } from "./UserProvider";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <UserProvider>
+        <Home />
+      </UserProvider>
     </div>
   );
 }
---
+++ client/src/components/RequestDashboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RequestDashboard() {
+  return <div>RequestDashboard</div>;
+}
+
+export default RequestDashboard;
