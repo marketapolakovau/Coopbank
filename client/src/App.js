3dd13f5e78b8e2ec8dba2545b6df71a276a566d0
marketapolakovau <marketa.polakova@unicorn.com>
2022-09-21 15:56:07 +0200

    bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
-import "bootstrap/dist/css/bootstrap.min.css";
 import PersonalDataForm from "./components/PersonalDataForm";
 
-import Calculator from './components/Calculator'
-
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div className="App">
-
       <Home />
 
       <PersonalDataForm />
 
-        <Calculator />
-
-
+      <Calculator />
     </div>
   );
 }
