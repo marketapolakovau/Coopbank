5cbbc93819637363a4528a7dbca188697a75a07a
Jakub <jakubtaufer95@gmail.com>
2022-09-26 11:38:41 +0200

    jakub commit7

--- client/src/components/PersonalDataForm.js
+++ client/src/components/PersonalDataForm.js
@@ -3,6 +3,7 @@ import { Col, Form, Row } from "react-bootstrap";
 import IndividualForm from "./IndividualFrom";
 import OsvcForm from "./OsvcForm";
 import LegalEntityForm from "./LegalEntityForm";
+import styles from "../css/personal-data-form.css";
 
 function PersonalDataForm() {
   const [applicantType, setApplicantType] = useState("Fyzická osoba");
@@ -19,33 +20,36 @@ function PersonalDataForm() {
   };
 
   return (
-    <div className="container " style={{ maxWidth: "720px" }}>
-      <div style={{ backgroundColor: "#00843D" }}>
-        <h1 style={{ color: "#fff" }} className="text-center mt-4 mb-4">
-          Osobní udaje
-        </h1>
-        <Row>
-          <Col>
-            <h2 style={{ color: "#fff" }} className="text-center">
-              {applicantType}
-            </h2>
-          </Col>
-          <Col>
-            <Form.Select
-              className="text-center"
-              style={{ maxWidth: "300px" }}
-              onChange={(e) => setApplicantType(e.target.value)}
-            >
-              <option value="Fyzická osoba">Fyzická osoba</option>
-              <option value="Podnikatel">Podnikatel</option>
-              <option value="Právnická osoba">Právnická osoba</option>
-            </Form.Select>
-          </Col>
-        </Row>
-        <br />
+    <div>
+      <div className="formMain">
+        <div className="container maxWidth">
+          <div>
+            <h1 className="text-center mt-4 mb-4 headingPrimary">
+              Osobní udaje
+            </h1>
+            <Row>
+              <Col>
+                <h2 className="text-center headingSecondary">
+                  Půjčku sjednává
+                </h2>
+              </Col>
+              <Col>
+                <Form.Select
+                  className="text-center formSelect"
+                  onChange={(e) => setApplicantType(e.target.value)}
+                >
+                  <option value="Fyzická osoba">Fyzická osoba</option>
+                  <option value="Podnikatel">Podnikatel</option>
+                  <option value="Právnická osoba">Právnická osoba</option>
+                </Form.Select>
+              </Col>
+            </Row>
+          </div>
+        </div>
       </div>
-      <br />
-      <div>{displayFormType(applicantType)}</div>
+      <div className="container maxWidth marginTop">
+        {displayFormType(applicantType)}
+      </div>
     </div>
   );
 }
